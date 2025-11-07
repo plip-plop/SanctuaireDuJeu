0526a0444170118192ac595c620b718e6e63115e
unknown <emmanuel.kerjean@gmail.com>
2025-11-07 16:54:54 +0100

    TP 3

--- src/card/card.ts
+++ src/card/card.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Product } from './product';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
 export class Card {
-  product = {
-    id: 1,
-    title: 'Grand prix de Belcastel',
-    description:
-      "Faites avancer votre animal en le nourrissant et ainsi tenter de remporter la course du chaudron d'or !",
-    photo: 'assets/img/belcastel-xl.png',
-    price: 50,
-    stock: 2,
-  };
+  product = input.required<Product>();
+  addToBasket = output<Product>();
+
+  onClick() {
+    this.addToBasket.emit(this.product());
+  }
 }
---
+++ src/card/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  description: string;
+  title: string;
+  photo: string;
+  stock: number;
+  price: number;
+}
